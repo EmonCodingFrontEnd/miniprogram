5fefb17906b3c9d394191b952b516ccb30bdaa09
liming <mingli@ishanshan.com>
2018-05-05 17:09:21 +0800

    修改基础库为2.0.2，添加兼容性测试

--- app.js
+++ app.js
@@ -36,6 +36,16 @@ App({
   },
   onShow: function(options) {
     console.log("场景值：", options);
+    if (wx.openBluetoothAdapter) {
+      console.log("API可用");
+      wx.openBluetoothAdapter()
+    } else {
+      // 如果希望用户在最新版本的客户端上体验您的小程序，可以这样子提示
+      wx.showModal({
+        title: '提示',
+        content: '当前微信版本过低，无法使用该功能，请升级到最新微信版本后重试。'
+      })
+    }
   },
   globalData: {
     userInfo: null
